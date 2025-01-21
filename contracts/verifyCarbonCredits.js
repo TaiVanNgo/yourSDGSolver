df7a376b69ad9c62a07237d3259dd58c765e8f45
marciohardcore <s4026694@rmit.edu.vn>
2025-01-21 15:16:27 +0700

    waller

--- contracts/verifyCarbonCredits.js
+++ contracts/verifyCarbonCredits.js
@@ -20,3 +20,16 @@ const carbonCredits = calculateCarbonCredits(data);
 verifyCarbonCredits(carbonCredits).then((result) => {
   console.log('Verification Result:', result);
 });
+
+const { ToucanSdk } = require('@toucanprotocol/toucan-sdk');
+const Web3 = require('web3');
+
+// Initialize Web3 and Toucan SDK
+const web3 = new Web3(new Web3.providers.HttpProvider('https://moonbeam.network'));
+const toucan = new ToucanSdk(web3);
+
+// Tokenize the carbon credits
+async function tokenizeCarbonCredits(carbonCredits) {
+    const tokenId = await toucan.tokenizeCarbonCredits(carbonCredits);
+    return tokenId;
+}
