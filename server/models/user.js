b4e0631941e39559c5c66c032e76348c16ee2e71
NhanNguyen20 <nguyenlethunhan@gmail.com>
2025-01-23 00:49:29 +0700

    update schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,59 @@
 const Mongoose = require("mongoose");
 const { Schema } = Mongoose;
 
+// Sub-schema for Projects
+const UserProjectSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  }
+}, { _id: false });
+
+// Sub-schema for Carbon Credit
+const CarbonCreditSchema = new Schema({
+  projects: {
+    type: [UserProjectSchema],
+    required: true,
+  },
+  totalCreditNumber: {
+    type: Number,
+    required: true,
+  }
+}, { _id: false });
+
+// Main User schema
 const UserSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   location: {
     type: String,
+    required: true,
   },
   walletAddress: {
     type: String,
+    required: true,
   },
   role: {
     type: String,
     default: "buyer",
-    enum: ["buyer", "seller"],
+    enum: ["buyer", "seller"]
   },
+  totalCredits: {
+    type: Number
+  },
+  carbonCredit: {
+    type: CarbonCreditSchema
+  }
 });
 
 module.exports = Mongoose.model("User", UserSchema);
